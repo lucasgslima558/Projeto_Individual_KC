5eab754b60db429c534eee8f2739e46b9bd79ce2
lucasgslima558 <lucas.slima@sptech.school>
2025-11-20 12:28:52 -0300

    WEB-DATA-VIZ - Limpeza no repositório e início da página de Música

--- web-data-viz/src/database/script-tabelas.sql
+++ web-data-viz/src/database/script-tabelas.sql
@@ -1,82 +1,102 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
-
-/*
-comandos para mysql server
-*/
-
-CREATE DATABASE aquatech;
-
-USE aquatech;
-
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
-);
+/* Script do Projeto Individual */
 
 CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+	id_usuario INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(90) NOT NULL,
+	username VARCHAR(20) NOT NULL,
+    senha VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+CREATE TABLE album(
+	id_album INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(90) NOT NULL,
+    dtLancamento DATE
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+CREATE TABLE musica(
+	id_musica INT PRIMARY KEY AUTO_INCREMENT,
+    fk_album INT,
+    FOREIGN KEY (fk_album)
+		REFERENCES album (id_album),
+    nome VARCHAR(90) NOT NULL
 );
 
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
+CREATE TABLE avaliacao(
+	id_avaliacao INT PRIMARY KEY AUTO_INCREMENT,
+    fk_usuario INT,
+    FOREIGN KEY (fk_usuario)
+		REFERENCES usuario (id_usuario),
+    fk_musica INT,
+    FOREIGN KEY (fk_musica)
+		REFERENCES musica (id_musica),
+	nota DECIMAL(3,2) NOT NULL,
+    comentario TEXT(150)
 );
 
-INSERT INTO medida (dht11_umidade, dht11_temperatura, momento, fk_aquario) VALUES
-	(35, 18, now(), 1),
-	(37, 19, now(), 1),
-	(38, 20, now(), 1),
-	(45, 16, now(), 1),
-	(40, 21, now(), 1),
-	(40, 16, now(), 2),
-	(36, 20, now(), 2),
-	(39, 19, now(), 2),
-	(35, 25, now(), 2),
-	(32, 30, now(), 2);
+/* Inserção */
+
+INSERT INTO album (nome, dtLancamento) VALUES
+	('In the Court of the Crimson King', '1969-10-10'),
+    ('In the Wake of Poseidon', '1970-05-15'),
+    ('Lizard', '1970-12-11'),
+    ('Islands', '1971-12-03'),
+    ("Larks' Tongues in Aspic", '1973-03-23'),
+    ('Starless and Bible Black', '1974-03-29'),
+    ('Red', '1974-10-06'),
+    ('Discipline', '1981-10-02');
     
-SELECT CASE
-	WHEN dht11_umidade > 35 THEN 'Teste1'
-    WHEN dht11_umidade BETWEEN 25 AND 34 THEN 'Teste2'
-	ELSE 'Roberto'
-    END AS Medida
-FROM medida;
-	
+INSERT INTO musica (fk_album, nome) VALUES
+	(1, '21st Century Schizoid Man'),
+	(1, 'I Talk to the Wind'),
+	(1, 'Epitaph'),
+	(1, 'Moonchild'),
+	(1, 'The Court of the Crimson King'),
+	(2, 'Peace - A Beginning'),
+	(2, 'Pictures of a City'),
+	(2, 'Cadence and Cascade'),
+	(2, 'In the Wake of Poseidon'),
+	(2, 'Peace - A Theme'),
+	(2, 'Cat Food'),
+	(2, "The Devil's Triangle"),
+	(2, 'Peace - An End'),
+	(3, 'Cirkus'),
+	(3, 'Indoor Games'),
+	(3, 'Happy Family'),
+	(3, 'Lady of the Dancing Water'),
+	(3, 'Lizard'),
+	(4, 'Formentera Lady'),
+	(4, "Sailor's Tale"),
+	(4, 'The Letters'),
+	(4, 'Ladies of the Road'),
+	(4, 'Prelude: Song of the Gulls'),
+	(4, 'Islands'),
+    (5, "Larks' Tongues in Aspic, Part One"),
+    (5, 'Book of Saturday'),
+    (5, 'Exiles'),
+    (5, 'Easy Money'),
+    (5, 'The Talking Drum'),
+    (5, "Larks' Tongues in Aspic, Part Two"),
+    (6, 'The Great Deceiver'),
+    (6, 'Lament'),
+    (6, "We'll let you know"),
+    (6, 'The Night Watch'),
+    (6, 'Trio'),
+    (6, 'The Mincer'),
+    (6, 'Starless and Bible Black'),
+    (6, 'Fracture'),
+    (7, 'Red'),
+    (7, 'Fallen Angel'),
+    (7, 'One More Red Nightmare'),
+    (7, 'Providence'),
+    (7, 'Red'),
+    (8, 'Elephant Talk'),
+    (8, 'Frame by Frame'),
+    (8, 'Matte Kudasai'),
+    (8, 'Indiscipline'),
+    (8, 'Thela Hun Ginjeet'),
+    (8, 'The Sheltering Sky'),
+    (8, 'Discipline');
 
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
+/* Views */
+
+/* Consulta */
